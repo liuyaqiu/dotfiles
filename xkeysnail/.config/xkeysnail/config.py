b62906d0c92f6f5ddcbfdc36648bc4573e34ea08
liuyaqiu <liuyaqiu1993@gmail.com>
2022-11-23 12:21:21 +0800

    Add config of xkeysnail.

---
+++ xkeysnail/.config/xkeysnail/config.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+import re
+from xkeysnail.transform import *
+
+# [Global modemap]
+define_modmap({
+    Key.CAPSLOCK: Key.LEFT_CTRL,
+    Key.LEFT_CTRL: Key.CAPSLOCK
+})
---
+++ xkeysnail/README.md
@@ -0,0 +1,5 @@
+配置参考
+
+https://liuyaqiu.io/post/xkeysnail/
+
+https://www.aloxaf.com/2019/11/xkeysnail_tutorial/
